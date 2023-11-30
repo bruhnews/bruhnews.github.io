aef9210124dab2803e9ae8f7d63d13c17d41f63d
darcy <acednes@gmail.com>
2023-12-01 09:58:45 +1100

    use full qualified `URL_ROOT`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use ibex::{routes, ssg};
 
 use bruhnews::{ARTICLES, BEST_HEADLINES};
 
-const URL_ROOT: &str = "/";
+const URL_ROOT: &str = "https://bruh.news/";
 
 const REPO_URL: &str = "https://github.com/bruhnews/bruhnews.github.io";
 const SALE_URL: &str = "#"; //TODO
